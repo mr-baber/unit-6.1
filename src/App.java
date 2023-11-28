ef825e9a56cf3cfa573a2e138390e723c2a9ed7e
Ryan Baber <rbaber@episcopalacademy.org>
2023-11-28 10:31:19 -0500

    initial commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Card testCard = new Card(6, 2);
+        Die d6 = new Die();
+        System.out.println(testCard);
+        System.out.println(d6);
+
+    }
+}
